d98b03600914f54706626312dd9f9b016c6df077
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-02-07 14:27:16 +0600

    problem-04 updated

---
+++ problem-04.js
@@ -0,0 +1,25 @@
+function gonoVote(array) {
+    if(!Array.isArray(array)){
+return "Invalid"
+    }
+         let countHa=0;
+         let countNa=0;
+       for (let ans of array){
+        if(ans==='ha'){
+            countHa++
+        }
+        if(ans==='na'){
+            countNa++
+        }
+       }
+  if(countHa>countNa){
+    return true
+  }
+ if(countHa==countNa){
+    return "equal"
+ }
+ else {
+    return false
+ }
+ 
+}
